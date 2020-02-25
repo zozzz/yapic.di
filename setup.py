9612977259b6a2ecf3c73dadfcad4c1f2a2ed2bf
Vetési Zoltán <vetesi.zoltan@gmail.com>
2020-02-25 18:43:59 +0100

    chore(bump): 2.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, Extension
 from setuptools.command.test import test as TestCommand
 from setuptools import Command
 
-VERSION = "2.0.0"
+VERSION = "2.0.1"
 
 define_macros = {
     "YAPIC_DI_VERSION_MAJOR": VERSION.split(".")[0],
