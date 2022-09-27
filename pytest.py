d95f4c365759dc470ca9e50cd28198270c6a6bd8
Vetési Zoltán <zozzz@trigon.hu>
2022-09-27 14:01:41 +0200

    Fix github actions

--- pytest.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-args = ""
-if len(sys.argv) > 1:
-    args = "-- " + " ".join(sys.argv[1:])
-
-print("%s %s %s" % (sys.executable, "setup.py test", args))
-os.system("%s %s %s" % (sys.executable, "setup.py test", args))
